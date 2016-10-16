0088980a89cde2db4d4630992d6371128b5e57a2
zhangyf <zhangyf@mapbar.com>
2016-10-16 08:06:56 +0800

    util

---
+++ bluetoothlib/src/main/java/com/example/bluetoothlib/util/LogHelper.java
@@ -0,0 +1,10 @@
+package com.example.bluetoothlib.util;
+
+/**
+ * Created by zhangyunfei on 16/9/29.
+ */
+public class LogHelper {
+    public static void d(String tag, String msg) {
+        android.util.Log.d(tag, msg);
+    }
+}
